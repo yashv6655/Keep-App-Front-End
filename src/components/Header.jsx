f3e1d386f425587faeec8e1468b51dbe8ac88078
Yash Velagapudi <yashv6655@gmail.com>
2020-04-03 20:10:23 -0700

    Initial Commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import FlareIcon from "@material-ui/icons/Flare";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <FlareIcon />
+        Keeper
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
